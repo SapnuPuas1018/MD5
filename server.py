a45c49b495b1ef6099c9b8fc507427a13e539b1e
SapnuPuas1018 <yuvalhayun20077@gmail.com>
2024-09-28 19:32:54 +0300

    server is now a class (yay)

--- server.py
+++ server.py
@@ -1,66 +1,77 @@
-import logging
+"""
+The Python Class that implements the server
+"""
+
 import socket
 import threading
+from threading import Thread
+import hashlib
 from protocol import *
 
-IP = '127.0.0.1'
+# Socket Constants
+QUEUE_SIZE = 1
+IP = '0.0.0.0'
 PORT = 12345
-QUEUE_LEN = 20
-MAX_PACKET = 1024
-START_RANGE = 0
-STOP_RANGE = 10**10
-LAST = 0
+SOCKET_TIMEOUT = 2
 lock = threading.Lock()
-found = False
-
-def give_range(client_socket):
-    lock.acquire()
-    global LAST
-    send(client_socket, f'{LAST}-{LAST + 10 ** 6}')
-    print(f'i sent range: {LAST}-{LAST + 10 ** 6}')
-    LAST = LAST + 10 ** 6
-    lock.release()
-
-def handle_client(client_socket, client_address):
-    global found
-    number_of_cores = recv(client_socket)
-    print(f'number_of_cores: {number_of_cores}')
-    while True:
-        give_range(client_socket)
-        data = recv(client_socket)
-        if data == 'found':
-            print('found it')
-            original = recv(client_socket)
-            print('the original message is: ' + original)
-            found = True
-            break
-
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        server_socket.bind((IP, PORT))
-        server_socket.listen(QUEUE_LEN)
-        print("Listening for connections on port %d" % PORT)
 
 
-        global found
+class Server:
+    def __init__(self):  # Use double underscores for the constructor
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.thread_list = []  # Initialize thread_list here
+        self.found = False
+        self.decrypted_str = ''
+        self.last = 0
 
-        thread_list = []
-        while not found:
-            client_socket, client_address = server_socket.accept()
-            print('New connection received from ' + client_address[0] + ':' + str(client_address[1]))
+    def start_server(self):
+        try:
+            # Set up the server socket
+            self.server_socket.bind((IP, PORT))
+            self.server_socket.listen(QUEUE_SIZE)
+            print("Listening for connections on port %d" % PORT)
+            self.server_socket.settimeout(SOCKET_TIMEOUT)
 
-            thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-            thread.start()
-            thread_list.append(thread)
-            print(thread_list)
-            print(f'found: {found}')
-    except socket.error as err:
-        print('received socket error on client socket' + str(err))
+            # Client Accepting Loop
+            while not self.found:
+                try:
+                    client_socket, addr = self.server_socket.accept()
+                    print('New connection received from ' + addr[0] + ':' + str(addr[1]))
 
-    finally:
-        server_socket.close()
+                    thread = Thread(target=self.handle_client, args=(client_socket, addr))  # Fix arguments
+                    thread.start()
+                    self.thread_list.append(thread)
+                    print(self.found)
+                except socket.timeout:
+                    pass
+        except socket.error as err:
+            print('received socket exception - ' + str(err))
+        finally:
+            # Thread joining
+            for thread in self.thread_list:
+                thread.join()
 
+            self.server_socket.close()
+            return self.decrypted_str
 
-if __name__ == '__main__':
-    main()
+    def handle_client(self, client_socket, addr):
+        # Receive Work Request
+        number_of_cores = recv(client_socket)
+        print(f'number_of_cores: {number_of_cores}')
+        while True:
+            self.give_range(client_socket)
+            data = recv(client_socket)
+            if data == 'found':
+                print('found it')
+                self.decrypted_str = recv(client_socket)
+                print('the original message is: ' + self.decrypted_str)
+
+                self.found = True
+                break
+
+    def give_range(self, client_socket):
+        lock.acquire()
+        send(client_socket, f'{self.last}-{self.last + 10 ** 6}')
+        print(f'i sent range: {self.last}-{self.last + 10 ** 6}')
+        self.last += 10 ** 6
+        lock.release()
